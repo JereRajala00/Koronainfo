afee1fa6347ff680cdf6cf31ea3dc4387f77ade1
tiitustelke <tiitus.telke@metropolia.fi>
2020-04-20 17:52:28 +0300

    Tartuntatiedot -activityn layout

--- app/src/main/java/com/example/koronainfo/Tartuntatiedot.java
+++ app/src/main/java/com/example/koronainfo/Tartuntatiedot.java
@@ -3,12 +3,33 @@ package com.example.koronainfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Tartuntatiedot extends AppCompatActivity {
 
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tartuntatiedot);
+
+        /*lv = (ListView)findViewById(R.id.tartuntatiedotListView);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("testi");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(
+                this,
+                R.layout.listview_layout,
+                arrayList
+        );
+
+        lv.setAdapter(arrayAdapter);*/
+
+
     }
 }
